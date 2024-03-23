2384ab8aceaf38ee9c43371c980dff7e6406cc21
WendyKariuki <phaminuswendy@gmail.com>
2024-03-23 22:03:46 +0300

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    const distance = Math.abs(pickupLocation - hqLocation);
+    return distance;
+}
+console.log(distanceFromHqInBlocks(50)); 
+
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetPerBlock = 264;
+    const distanceInFeet = blocks * feetPerBlock;
+    return distanceInFeet;
+}
+console.log(distanceFromHqInFeet(50)); 
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocksTravelled = Math.abs(endBlock - startBlock);
+    const feetPerBlock = 264;
+    const distanceInFeet = blocksTravelled * feetPerBlock;
+    return distanceInFeet;
+}
+console.log(distanceTravelledInFeet(43, 48)); 
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocksTravelled = Math.abs(endBlock - startBlock);
+    const feetPerBlock = 264;
+    const distanceInFeet = blocksTravelled * feetPerBlock;
+    return distanceInFeet;
+}
+console.log(distanceTravelledInFeet(43, 48)); 
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let farePrice;
+
+    if (distance <= 400) {
+        farePrice = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        farePrice = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        farePrice = 25;
+    } else {
+        farePrice = 'cannot travel that far';
+    }
+
+    return farePrice;
+}
+console.log(calculatesFarePrice(43, 48)); 
+
